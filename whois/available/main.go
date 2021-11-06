10f927d9a64150a95ec32942909ebfc9acb37e7f
AEON-MALL <jbytq@yahoo.co.jp>
2021-11-06 22:01:42 +0900

    fourth commit

--- whois/available/main.go
+++ whois/available/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"log"
 	"net"
+	"os"
 	"strings"
+	"time"
 )
 
 func exists(domain string) (bool, error) {
@@ -21,4 +25,20 @@ func exists(domain string) (bool, error) {
 		}
 	}
 	return true, nil
+}
+
+var marks = map[bool]string{true: "使用可", false:"使用済"}
+
+func main(){
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan(){
+		domain := s.Text()
+		fmt.Print(domain," ")
+		exist , err := exists(domain)
+		if err != nil{
+			log.Fatalln(err)
+		}
+		fmt.Println(marks[!exist])
+		time.Sleep(1* time.Second)
+	}
 }
