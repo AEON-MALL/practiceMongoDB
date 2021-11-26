b40595212aa765b52bfc27ff0b1fd3d7994bc248
AEON-MALL <jbytq@yahoo.co.jp>
2021-11-26 23:33:41 +0900

    eight commit

---
+++ MongoDB/api/respond.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func decodeBody(r *http.Request, v interface{}) error{
+	defer r.Body.Close()
+	return json.NewDecoder(r.Body).Decode(v)
+}
+
+func encodeBody(w http.ResponseWriter, r *http.Request, v interface{}) error{
+	return json.NewEncoder(w).Encode(v)
+}
+
+func respond(w http.ResponseWriter, r *http.Request, status int , data interface{}){
+	w.WriteHeader(status)
+	if data != nil{
+		encodeBody(w,r,data)
+	}
+}
